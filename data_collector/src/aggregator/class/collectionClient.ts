d4c9877af67209139f19ccd0afdbc77df3ec608a
Markus Gude <progamer101101@gmx.de>
2020-03-30 14:21:52 +0200

    aggregator is growing

--- data_collector/src/aggregator/class/collectionClient.ts
+++ data_collector/src/aggregator/class/collectionClient.ts
@@ -1,6 +1,7 @@
 import { logger } from "../helpers";
 import AWS from "aws-sdk";
 import { userInfo } from "os";
+import { Aggregation } from "../models";
 
 export class AggregatorIntensivBettenClient {
   private aggregated_last;
@@ -11,38 +12,56 @@ export class AggregatorIntensivBettenClient {
       "aggregated.json"
     );
 
-    let register = await this.aggregateIntensivBettenRegister();
-    let capacity = await this.aggregateIntensivBettenCapacity();
+    let register = await this.aggregateIntensivBettenAWS("register");
+    let capacity = await this.aggregateIntensivBettenAWS("capacity");
 
-    return ["test"];
+    return await this.mergeData(register, capacity);
   }
+
   /**
    * Aggregates intensiv betten register
    * @returns intensiv betten register
    */
-  public async aggregateIntensivBettenRegister(): Promise<Array<any>> {
+  private async aggregateIntensivBettenAWS(type: string): Promise<Array<any>> {
     // Schau nach wann der letzte update auf den aggregated.json war
-    let aggregated_last_timestamp = await this.aggregated_last.last_update;
-
-    // Lass dir eine Liste an allen dateien geben
-    let bucket_list: Array<any> = this.getS3BucketList(
-      "intensiv-betten-prod/data_storage/register"
+    let aggregated_last_timestamp = Date.parse(
+      await this.aggregated_last.last_update
     );
 
+    // Lass dir eine Liste an allen dateien geben
+    let bucket_list = await this.getS3BucketList("intensiv-betten-prod", type);
+
     // Beschränke die Liste auf Dateien die jünger sind als aggregated.json
-    bucket_list = bucket_list.map(element => {
-      logger.info(element);
+    bucket_list = bucket_list.filter(element => {
+      let current_timestamp = Date.parse(
+        element.Key.substring(22, element.Key.length - 5)
+      );
+      if (current_timestamp <= aggregated_last_timestamp) {
+        return false;
+      }
+      return true;
     });
     // hol dir alle daten in einem array und geb das Zurück
 
-    return ["test"];
+    let files = [];
+
+    await Promise.all(
+      bucket_list.map(async element => {
+        files.push({
+          time: Date.parse(element.Key.substring(22, element.Key.length - 5)),
+          data: await this.getS3BucketFile("intensiv-betten-prod", element.Key)
+        });
+      })
+    );
+
+    return files;
   }
 
   /**
    * Aggregates intensiv betten capacity
    * @returns intensiv betten capacity
    */
-  public async aggregateIntensivBettenCapacity(): Promise<Array<any>> {
+  private async aggregateIntensivBettenCapacity(): Promise<Array<any>> {
     return ["test"];
   }
 
@@ -51,27 +70,52 @@ export class AggregatorIntensivBettenClient {
    * @param bucket
    * @param file
    */
-  public getS3BucketFile(bucket, file) {
+  private getS3BucketFile(bucket, file) {
     var s3 = new AWS.S3();
-    s3.getObject({ Bucket: bucket, Key: file }, function(error, data) {
-      if (error != null) {
-        logger.info("Failed to retrieve an object: " + error);
-      } else {
-        logger.info("Loaded " + data.ContentLength + " bytes");
-        return data.Body;
-      }
+    var self = this;
+
+    return new Promise(function(resolve, reject) {
+      s3.getObject({ Bucket: bucket, Key: file }, function(error, data) {
+        if (error != null) {
+          logger.info("Failed to retrieve an object: " + error);
+          reject(error);
+        } else {
+          logger.info("Loaded " + data.ContentLength + " bytes");
+          resolve(JSON.parse(data.Body.toString()));
+        }
+      });
     });
   }
 
-  private getS3BucketList(bucket): any {
+  /**
+   * Gets s3 bucket list
+   * @param bucket
+   * @param type
+   * @returns s3 bucket list
+   */
+  private getS3BucketList(bucket, type): any {
     var s3 = new AWS.S3();
-    s3.listObjects({ Bucket: bucket }, function(error, data) {
-      if (error != null) {
-        logger.info("Failed to retrieve an object: " + error);
-      } else {
-        logger.info("Loaded list");
-        return data;
-      }
+    var self = this;
+    var params = {
+      Bucket: bucket,
+      Prefix: "data_storage/" + type
+    };
+
+    return new Promise(function(resolve, reject) {
+      s3.listObjectsV2(params, function(error, data) {
+        if (error != null) {
+          logger.info("Failed to retrieve an object: " + error);
+          reject(error);
+        } else {
+          logger.info("Loaded list");
+          resolve(data.Contents);
+        }
+      });
     });
+  }
+
+  private mergeData(register, capacity): Aggregation {
+    let aggregation: Aggregation;
+    return aggregation;
   }
 }
--- data_collector/src/aggregator/models/models.ts
+++ data_collector/src/aggregator/models/models.ts
@@ -1,1 +1,29 @@
-export interface diviEntry {}
+export interface Value {
+  icu_low_care: string;
+  icu_high_care: string;
+  ecmo: string;
+  covid: number;
+}
+
+export interface History {
+  date: string;
+  value: Value;
+}
+
+export interface Datum {
+  hospital: string;
+  contact: string;
+  fed: string;
+  icu_low_care: string;
+  icu_high_care: string;
+  ecmo: string;
+  lat: number;
+  lon: number;
+  covid_current: number;
+  history: History[];
+}
+
+export interface Aggregation {
+  last_update: string;
+  data: Datum[];
+}

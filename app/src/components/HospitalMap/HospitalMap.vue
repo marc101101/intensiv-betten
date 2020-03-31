99595123572562bf38dffd01bab5ccb2daa8eaf3
Jakob Niggel <jakobniggel@gmail.com>
2020-03-31 16:37:04 +0200

    add-map

---
+++ app/src/components/HospitalMap/HospitalMap.vue
@@ -0,0 +1,44 @@
+<template>
+  <GmapMap
+    :center="{ lat: 49.0134297, lng: 12.1016236 }"
+    :zoom="12"
+    style="width: 100%; height: 100%"
+    :options="{
+      streetViewControl: false,
+      fullscreenControl: false,
+      mapTypeControl: false,
+      styles: mapStyle
+    }"
+  >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in hospitals"
+      :position="m.position"
+      :icon="m.icon"
+      :clickable="true"
+    />
+  </GmapMap>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import mapStyle from "./mapStyle";
+
+export default Vue.extend({
+  name: "HospitalMap",
+
+  data: () => ({
+    mapStyle,
+    hospitals: [
+      {
+        position: { lat: 49, lng: 12 },
+        icon: {
+          url:
+            "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+        }
+      }
+    ]
+  })
+});
+</script>
---
+++ app/src/plugins/maps.ts
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCHSgM2AXU8ue0kDi4Zu_vSjoh2wQFhti4"
+  }
+});

a771bda48ba219d30968d039f4b46399a0138643
Markus Gude <progamer101101@gmx.de>
2020-04-06 16:51:33 +0200

    Revert "Merge branch 'bugfix/collector_v2'"
    This reverts commit 9076b6a502919b5a33da9c3cec58e1a11c263c21.

--- data_collector/src/v1/collector/helpers/lib.ts
+++
@@ -1,9 +0,0 @@
-import { default as Pino } from "pino";
-
-const loggerConfig = {
-    level: process.env.LOG_LEVEL || "info",
-    prettyPrint: true,
-};
-
-export const logger = Pino(loggerConfig, process.stderr);
-logger.info("LOGLEVEL: " + loggerConfig.level);
